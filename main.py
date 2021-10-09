0b05648f80194c97d07b15386f650bfe41e17af9
leowwwww <1769519804@qq.com>
2021-10-09 18:20:58 +0800

    new

--- main.py
+++ main.py
@@ -5,8 +5,12 @@ from train_test_data_struct import train_test
 import time
 
 if __name__ == '__main__':
-    bias = []
-    weight = []
+    bias_con1 = []
+    weight_com1 = []
+    bias_con2 = []
+    weight_con2 = []
+    bias_lin = []
+    weight_lin = []
     start = time.time()
     train_loader , x_test , y_test = train_test(0)
     a , b , c = run(train_loader=train_loader , x_test= x_test , y_test= y_test , index=0)
@@ -22,5 +26,9 @@ if __name__ == '__main__':
     
     print(b[0])
     print(b[1])
+    print(c[0])
+    print(c[1])
+    print(b[0][1][0][0][4][4].item())
+    print(c[0][1][0][43].item())
     end = time.time()
     print('耗时：',end-start)
