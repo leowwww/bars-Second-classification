d0cbf6dcad10595bb724f6a890a71229c17aefab
leowwwww <1769519804@qq.com>
2021-10-13 11:17:45 +0800

    最终结果

--- new_model.py
+++ new_model.py
@@ -1,4 +1,5 @@
 import torch
+from torch._C import *
 import torch.nn as nn
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
@@ -54,54 +55,32 @@ class NET(nn.Module):
                       [ -0.0162, -0.0499, -0.1062, 0.0586, 0.0572, 0.0031, 0.012, 0.0606, 0.0047, -0.1035, 0.0433, -0.1051, 0.017, -0.1431, -0.073, -0.0267, -0.1493, -0.1305, -0.1046, 0.1276, 0.039, -0.0864, -0.0882, -0.1325, -0.0871, -0.1381, -0.0318, -0.1371, 0.0882, -0.0572, -0.117, -0.1246, 0.0955, -0.0718, -0.0901, -0.1301, 0.0331, -0.0968, 0.1065, -0.1325, -0.0167, -0.0824, -0.1378, 0.0088]]          
         self.weight_lin =  nn.Parameter(data= torch.FloatTensor(kernel_lin), requires_grad=False)
         self.bias_lin = nn.Parameter(data = torch.FloatTensor([0.424 , -0.5613]),requires_grad=False )
+        
+        #######################构造网络层
+        '''self.conv1 = F.conv2d(x, self.weight_con1,self.bias_con1, stride = 1,padding=2)
+        self.conv2 = F.conv2d(x,self.weight_con2 , self.bias_con2 , stride=1 , padding=2)
+        self.out = F.linear(x,self.weight_lin , self.bias_lin)'''
+        self.relu = nn.ReLU()
+        self.maxpool = nn.MaxPool2d(kernel_size=2)
 
     def forward(self, x):
+        #print(x.shape)
         x = F.conv2d(x, self.weight_con1,self.bias_con1, stride = 1,padding=2)
-        x = F.conv2d(x , self.weight_con2 , self.bias_con2 , stride=1 , padding=2)
-        return x
-    def par(self):
-        params_cnn1 = list(self.conv1.named_parameters())
-        '''print(params_cnn1.__len__())
-        print(params_cnn1[0])
-        print(params_cnn1[1])'''
-        params_cnn2 = list(self.conv2.named_parameters())
-        '''print(params_cnn2.__len__())
-        print(params_cnn2[0])
-        print(params_cnn2[1])'''
-        params_linear = list(self.out.named_parameters())
-        '''print(params_linear[0])
-        print(params_linear[1])'''
-        return params_cnn1 , params_cnn2 , params_linear
+        x = self.relu(x)
+        x = self.maxpool(x)
+        #print(x.shape)
+        x = F.conv2d(x,self.weight_con2 , self.bias_con2 , stride=1 , padding=2)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        x = x.view(x.size(0), -1)
+        #print(x.shape)
+        out = F.linear(x,self.weight_lin , self.bias_lin)
+        return out
 
-def run(train_loader,x_test , y_test , index):
+def run(x_test , y_test):
     cnn = NET()
-    optimizer = torch.optim.Adam(cnn.parameters(),lr = 0.01)
-    loss_func = nn.CrossEntropyLoss()
-    ######训练开始
-    for epoch in range(2):
-        correct = 0
-        for i,data in enumerate(train_loader):
-            inputs , lable = data
-            inputs = Variable(inputs)
-            lable = Variable(lable).long()
-            inputs = Variable(torch.unsqueeze(inputs, dim=0).float(), requires_grad=False)
-            output = cnn(inputs)
-            loss = loss_func(output , lable)
-            #print(loss)
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            predicted = torch.max(output.data,1)[1]
-            correct += (predicted == lable).sum()
-            #print(correct)
-            if i % 100 == 0:
-                print('Epoch :{}[{}/{}({:.0f}%)]\t Loss:{:.6f}\t Accuracy:{:.3f}'.format(epoch,i * len(inputs),len(train_loader.dataset),100.*i / len(train_loader),loss.data.item(),float(correct)/(i+1)))
-    ########################训练结束
-    #######################保存模型
-    torch.save(cnn,'modle\\'+str(index) + '.pt')
     ##############3#########测试开始
     y_pred = []
-    cnn.par()
     for i in range(len(x_test)):
         test_x = x_test[i]
         test_x = torch.from_numpy(test_x)
@@ -117,12 +96,17 @@ def run(train_loader,x_test , y_test , index):
         if y_test[i] == y_pred[i]:
             result += 1
     print('->>>>>>>>>>>>>>>>>>>>>>>>>>>准确率：{}'.format(result/len(y_test)))
-    a , b , c = cnn.par()
-    return a,b ,c , result/len(y_test)
+    return result/len(y_test)
     
 
 
 if __name__ =='__main__':
-    #######################自定义改变网络层的权重
-    bias_lin = nn.Parameter(data = torch.FloatTensor([0.424 , -0.5613]),requires_grad=False )
-    print(bias_lin)
+    train_loader , x_test , y_test = train_test(0)
+    result = run(x_test,y_test)
+    acc = []
+    for i in range(100):
+        train_loader , x_test , y_test = train_test(i)
+        result = run(x_test , y_test)
+        acc.append(result)
+    ACC = pd.DataFrame(acc)
+    ACC.to_excel('new_model\\accuracy.xlsx')
